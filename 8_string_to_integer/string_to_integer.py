6af9840fe04e547ab0e293653a3c9bf6f6acdca8
swimminginlove <cureoglu4@gmail.com>
2020-08-19 15:24:26 -0500

    string to int

--- 8_string_to_integer/string_to_integer.py
+++ 8_string_to_integer/string_to_integer.py
@@ -16,29 +16,38 @@ class Solution(object):
         if len(arr) == 0:
             return 0
 
-        # if array has sign in front then note the sign
+        # if array has sign in front then note the sign and delete it
         if arr[0] == '-':
             sign = -1
+            i = 1
+            # or del arr[0]
+
+        elif arr[0] == '+':
+            sign = 1
+            i = 1
+            # or del arr[0]
+
         else:
             sign = 1
 
-        # if array has a sign in front then we need to delete the sign from array
-        if arr[0] == '-' or arr[0] == '+':
-            del arr[0]
-            # or i = 1
-
-        # Iterate while i is less than arr array and arr[i] is a digit
+        # Iterate while _i_ less than _arr_ array and _arr[i]_ is a digit
         while i < len(arr) and arr[i].isdigit():
             # convert digits one by one using modulus and adding to result * 10
             # (4879) 4 > 48 > 487 > 4879
             # We could do int(arr[i]) but int conversion is slower. Also 48 is ord('0')
-            result = result*10 + ord(arr[i]) - 48
-            i += 1  # increment i
 
-        # multiply result by sign of s
+            #print("result: ", result, "arr[i]: ", arr[i])
+
+            # get ascii value of character at index _i_ of array _arr_.
+            # Also 48 is ord('0')
+            result = result*10 + ord(arr[i]) - 48
+            # increment i
+            i += 1
+
+        # multiply _result_ by sign of input _s_
         result = result * sign
 
-        # check if result less than or greater than +/- 2**32
+        # check if _result_ less than or greater than +/- 2**32
         if result < -2**31:
             return -2**31
         elif result > 2**31 - 1:
